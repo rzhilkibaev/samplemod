87eb4ffca6876e08613dff93be2536699ab29a7d
Renat <rzhilkibaev@gmail.com>
2016-08-14 15:45:09 -0700

    Log change event

--- shmenkins/handle_webhook.py
+++ shmenkins/handle_webhook.py
@@ -2,8 +2,16 @@
 import logging
 
 
+def get_repo_names():
+    return ["rzhilkibaev/demo-simple-web"]
+
 def handle(event, context):
     log = logging.getLogger()
     log.setLevel(logging.DEBUG)
     log.debug("received event " + str(event))
+    
+    repo_name = event["repository"]["full_name"]
+    if repo_name in get_repo_names():
+        log.info("repository " + repo_name + " changed")
+    
     return None
